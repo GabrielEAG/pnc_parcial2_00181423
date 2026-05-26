df58120691550d7ea306b63ed790056df32de03c
GabrielEAG <00181423@uca.edu.sv>
2026-05-25 18:55:39 -0600

    Primer commit

---
+++ Parcial2N_Capas/src/main/java/com/example/parcial2n_capas/Parcial2NCapasApplication.java
@@ -0,0 +1,13 @@
+package com.example.parcial2n_capas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Parcial2NCapasApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Parcial2NCapasApplication.class, args);
+    }
+
+}
---
+++ Parcial2N_Capas/src/test/java/com/example/parcial2n_capas/Parcial2NCapasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.parcial2n_capas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Parcial2NCapasApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
